b69d6b7be9364f8f9dd98995bd400af175ec8e82
amanthokale <amanthokale1137@gmail.com>
2023-04-14 21:05:44 +0530

    misc

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-User management module
+Administration module
+api documentation - https://documenter.getpostman.com/view/19239269/2s93Xx1QBA
--- src/Routes/menuRoute.js
+++ src/Routes/menuRoute.js
@@ -1,25 +1,31 @@
 const menuController = require("../Controllers/menuController");
-const USERS_ROUTE_BASE_URL = process.env.USERS_ROUTE_BASE_URL;
+const ADMINISTRATION_ROUTE_BASE_URL = process.env.ADMINISTRATION_ROUTE_BASE_URL;
 require("dotenv").config();
 
-console.log("USERS_ROUTE_BASE_URL --->", USERS_ROUTE_BASE_URL);
+console.log(
+  "ADMINISTRATION_ROUTE_BASE_URL --->",
+  ADMINISTRATION_ROUTE_BASE_URL
+);
 
 module.exports = (app) => {
   app.post(
-    "/" + USERS_ROUTE_BASE_URL + "/addMenuItems",
+    "/" + ADMINISTRATION_ROUTE_BASE_URL + "/addMenuItems",
     menuController.addMenuItems
   );
-  app.get("/" + USERS_ROUTE_BASE_URL + "/getMenu", menuController.getMenu);
+  app.get(
+    "/" + ADMINISTRATION_ROUTE_BASE_URL + "/getMenu",
+    menuController.getMenu
+  );
   app.post(
-    "/" + USERS_ROUTE_BASE_URL + "/updateMenuItemPrice",
+    "/" + ADMINISTRATION_ROUTE_BASE_URL + "/updateMenuItemPrice",
     menuController.updateMenuItem
   );
   app.post(
-    "/" + USERS_ROUTE_BASE_URL + "/addMenuItem",
+    "/" + ADMINISTRATION_ROUTE_BASE_URL + "/addMenuItem",
     menuController.addMenuItem
   );
   app.post(
-    "/" + USERS_ROUTE_BASE_URL + "/deleteMenuItem",
+    "/" + ADMINISTRATION_ROUTE_BASE_URL + "/deleteMenuItem",
     menuController.deleteMenuItem
   );
 };
